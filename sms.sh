9037c2e00843538a72f3a704db861e8f447a2c1e
bulenterkaltr <97557273+bulenterkaltr@users.noreply.github.com>
2024-03-16 00:43:46 +0400

    Update sms.sh
    SMS message changed

--- sms.sh
+++ sms.sh
@@ -12,7 +12,7 @@ do
 			do
 
 				sleep 3
-				termux-sms-send -n [$line]  "\Hi, Testing"
+				termux-sms-send -n [$line]  "\200 TRY yatırıma SweetBonanza 50 FreeSpin hediye. Jeton değeri 5 TRY. https://rebrand.ws/slotparkguncel"
 		#		sed -i '1d' "${fname}"
 				termux-toast "Done $i in $j seconds"
 				count=$(wc -l <$fname)
